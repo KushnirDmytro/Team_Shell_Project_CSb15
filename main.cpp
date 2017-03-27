a814ef9849f667d0042c9431ddb07f7c898f2e59
d1md1m <kushnir.dmytro.93@gmail.com>
2017-03-28 02:53:31 +0300

    final space in arg bug fixed Interpreter object provided Splitter object provided From-file interpretation performed

--- main.cpp
+++ main.cpp
@@ -39,6 +39,8 @@ int my_sh(size_t nargs, char **args);
 
 User *default_user;
 
+class Interpreter;
+
 class FileLaneIterator;
 
 
@@ -103,18 +105,158 @@ public:
     }
 };
 
-class Interpreter{
 
-    int proceed_sting(string* values){
+class Line_splitter{
+public:
+    Line_splitter(){
+
+    }
+
+    // left it here = line delimiters " \t\r\n\a"
+    vector <string> my_split_line(string input_str)
+    {
+        input_str.append(" ");
+        vector<string>args;
+
+        string delim = " "; //for delimer
+        size_t pos = 0;
+        string tok; // for toke
+
+        unsigned long start;
+
+        start = input_str.find_first_not_of(" ");
+
+        input_str = input_str.substr(start);
+
+        //cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" <<endl;
+        while ((pos = input_str.find(delim)) != string::npos) {
+            tok = input_str.substr(0, pos);
+            //  cout << tok << endl;
+            args.push_back(tok);
+            input_str.erase(0, pos + delim.length());
+        }
+        return args;
+    }
+
+
+//produced Kovalchuk, Refactored & extracted by Kushnir
+    int str_vector_to_chars(vector<string> *args, char** cargs){
+        size_t i;
+
+        for(i = 0; i < args->size(); ++i)
+        {
+            cargs[i] = new char[(*args)[i].size() + 1];
+            strcpy(cargs[i], (*args)[i].c_str());
+        }
+        cargs[i] = NULL;
+        return 0;
+    }
+
+
+
+
+    ~Line_splitter(){
+
+    }
+};
+
+
+class Interpreter{
+private:
+    Line_splitter *splitter;
+public:
+    Interpreter(){
+        this->splitter = new Line_splitter();
+    }
+
+
+    // launcher for custom modules
+    int my_extern_launcher(char **args)
+    {
+        pid_t pid, wpid;
+        int status;
+
+        pid = fork();
+        if (pid == 0) {
+            //  we are in Child process
+            if (execv(args[0], args) == -1) {
+                perror("my_Shell failed to launch this file");
+            }
+            exit(EXIT_FAILURE);
+        } else if (pid < 0) {
+            // Error forking
+            perror("my_Shell failed to fork");
+        } else {
+            // Parent process
+            do {
+                wpid = waitpid(pid, &status, WUNTRACED);
+                /*
+                 * WUNTRACED
+                 * also return if a child has stopped (but not traced via ptrace(2)).
+                 * Status for traced children which have stopped is provided even if this option is not specified.
+                 * WIFEXITED(status)
+                 * returns true if the child terminated normally, that is, by calling exit(3) or _exit(2),
+                 * or by returning from main().
+                 * WIFSIGNALED(status)
+                 * returns true if the child process was terminated by a signal.
+                 */
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status)); //checks for valid scenarios of exiting (we borrowed it)
+        }
+
         return 1;
     }
 
+
+    int num_my_builtins() {
+        return (int) embedded_lib.size();
+    }
+
+    int my_execute(vector<string> args)
+    {
+
+        char** cargs = new char*[args.size() + 1];
+        unsigned int args_number = (int) args.size();
+
+
+        cout << "NUMBER OF ARGS FOUND: " <<args_number << endl;
+        splitter->str_vector_to_chars(&args , cargs);
+
+        cout<< "builtIns #" << num_my_builtins() << endl;
+
+        for (int i = 0; i < num_my_builtins(); i++) {
+//        cout<< "builtIn #" << i << " = "<< my_builtin_str[i] <<endl;
+            auto search_iter = embedded_lib.find(cargs[0]);
+            if (search_iter != embedded_lib.end() ) // case when we have such a func in our lib
+            {
+                return (search_iter->second)->call(args_number, cargs);
+            }
+        }
+
+        return my_extern_launcher(cargs);
+
+
+    }
+
+    int proceed_sting(string* values){
+
+        vector<string> args = splitter->my_split_line(*values);
+
+
+        return my_execute(args);
+    }
+
+    ~Interpreter(){
+
+    }
     //TODO check for function options
     //TODO filemasks
 };
 
 
 
+Interpreter* default_interpreter;
+
+Line_splitter* def_line_split;
 
 Directory *current_directory;
 
@@ -125,16 +267,9 @@ ConsoleView *console;
 using  callable_function =  int (*)(size_t, char **);
 
 
-
 // forward declarations of built-in commands
 
 map <string, Embedded_func*> embedded_lib;
-
-
-
-int num_my_builtins() {
-    return (int) embedded_lib.size();
-}
 
 
 int my_pwd(size_t nargs, char **args)
@@ -143,8 +278,6 @@ int my_pwd(size_t nargs, char **args)
     printf("%s", current_directory->getActual_path().c_str());
     return 1;
 }
-
-
 
 
 int my_cd(size_t nargs, char **args)
@@ -204,10 +337,16 @@ int my_sh(size_t nargs, char **args)
             string st;
             cout << "------------FILE READING IN PROCESS------------------" << endl;
             int i =0;
+            int status;
             while(iter->fileIsReady()){
                 iter->getNextString(&st);
                 printf("String #%d red \n", i++);
                 cout << st << endl;
+               // st.append(" ");
+                status = default_interpreter->proceed_sting(&st);
+                if (!status){
+                    return 0;
+                };
 
             }
         }
@@ -218,6 +357,7 @@ int my_sh(size_t nargs, char **args)
             while(iter->fileIsReady()){
                 iter->getNextString(&st);
                 cout << st << endl;
+                return default_interpreter->proceed_sting(&st);
             }
         }
         else{
@@ -232,115 +372,15 @@ int my_sh(size_t nargs, char **args)
 
 
 
-// launcher for custom modules
-int my_extern_launcher(char **args)
-{
-    pid_t pid, wpid;
-    int status;
-
-    pid = fork();
-    if (pid == 0) {
-        //  we are in Child process
-        if (execv(args[0], args) == -1) {
-            perror("my_Shell failed to launch this file");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        // Error forking
-        perror("my_Shell failed to fork");
-    } else {
-        // Parent process
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-            /*
-             * WUNTRACED
-             * also return if a child has stopped (but not traced via ptrace(2)).
-             * Status for traced children which have stopped is provided even if this option is not specified.
-             * WIFEXITED(status)
-             * returns true if the child terminated normally, that is, by calling exit(3) or _exit(2),
-             * or by returning from main().
-             * WIFSIGNALED(status)
-             * returns true if the child process was terminated by a signal.
-             */
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status)); //checks for valid scenarios of exiting (we borrowed it)
-    }
-
-    return 1;
-}
-
-
-//produced Kovalchuk, Refactored & extracted by Kushnir
-int str_vector_to_chars(vector<string> *args, char** cargs){
-    size_t i;
-
-    for(i = 0; i < args->size(); ++i)
-    {
-        cargs[i] = new char[(*args)[i].size() + 1];
-        strcpy(cargs[i], (*args)[i].c_str());
-    }
-    cargs[i] = NULL;
-    return 0;
-}
-
-int my_execute(vector<string> args)
-{
-
-    char** cargs = new char*[args.size() + 1];
-    unsigned int args_number = (int) args.size();
-
-
-    cout << "NUMBER OF ARGS FOUND: " <<args_number << endl;
-    str_vector_to_chars(&args , cargs);
-
-    cout<< "builtIns #" << num_my_builtins() << endl;
-
-    for (int i = 0; i < num_my_builtins(); i++) {
-//        cout<< "builtIn #" << i << " = "<< my_builtin_str[i] <<endl;
-        auto search_iter = embedded_lib.find(cargs[0]);
-        if (search_iter != embedded_lib.end() ) // case when we have such a func in our lib
-         {
-            return (search_iter->second)->call(args_number, cargs);
-         }
-    }
-
-    return my_extern_launcher(cargs);
-
-
-}
-
 
 string my_read_line(void)
 {
     string buffer;
     getline(cin,buffer);
-    buffer+=" ";
+    //buffer+=" ";
     return buffer;
 }
 
-// left it here = line delimiters " \t\r\n\a"
-vector <string> my_split_line(string input_str)
-{
-    vector<string>args;
-
-    string delim = " "; //for delimer
-    size_t pos = 0;
-    string tok; // for toke
-
-    unsigned long start;
-
-    start = input_str.find_first_not_of(" ");
-
-    input_str = input_str.substr(start);
-
-    //cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" <<endl;
-    while ((pos = input_str.find(delim)) != string::npos) {
-        tok = input_str.substr(0, pos);
-      //  cout << tok << endl;
-        args.push_back(tok);
-        input_str.erase(0, pos + delim.length());
-    }
-    return args;
-}
 
 //execuion loop
 void my_loop(void)
@@ -353,8 +393,9 @@ void my_loop(void)
     do {
         console->display_all();
         line = my_read_line();
-        args = my_split_line(line);
-        status = my_execute(args); //if 0 - finished, exited
+        status = default_interpreter->proceed_sting(&line);
+        //args = my_split_line(line);
+        //status = my_execute(args); //if 0 - finished, exited
 
     } while (status);
 }
@@ -381,18 +422,12 @@ int main(int argc, char **argv)
 
 
     default_user = new User();
-
+    default_interpreter = new Interpreter();
+    def_line_split = new Line_splitter();
     //init_user(&this_user);
     current_directory = new Directory();
     console = new ConsoleView(current_directory);
-/*
-    char* test[] ={
-            "mysh",
-            "/home/d1md1m/CLionProjects/Lab_2_shell/testfiel.msh"
-    };
-  */
-    // Load config files, if any.
-//    my_sh(2, test);
+
     // Run command loop.
     my_loop();
 
