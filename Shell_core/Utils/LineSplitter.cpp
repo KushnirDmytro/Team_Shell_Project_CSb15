607d157d52574aa5dc839b0ccf4042b545feb749
d1md1m <kushnir.dmytro.93@gmail.com>
2017-06-18 19:38:58 +0300

    first prototype with stringstream

--- Shell_core/Utils/LineSplitter.cpp
+++ Shell_core/Utils/LineSplitter.cpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <boost/filesystem.hpp>
 #include <sstream>
+#include <iostream>
 #include "../EmbeddedFunc.h"
 
 
@@ -88,45 +89,204 @@ namespace sh_core {
             bool isIndependant(){
                 return !(isComment||isDoubleBrace || isBrace || isReverceBrace);
             }
+
+            char getToken(){
+                return 's';
+            }
         };
+
+        inline bool isSpace(char ch) {
+            return (ch == ' ' || ch == '\t'); // Yes TABS are allso Spaces, HOLY WAR is over
+        }
 
         const vector<string> LineSplitter::mySplitLine( string *input_str) const {
 
+            std::stringstream ss(*input_str);
 
-            /*
-            std::vector <std::pair<std::string, char>> *processing_buffer = new std::vector <std::pair<std::string, char>>;
+            std::vector <std::pair<std::string, char>> *tokens_vector = new std::vector <std::pair<std::string, char>>;
 
-            const std::string special_symbols = "'\"`%/#|><._* &=";
+            const std::string special_symbols = "'\"`%/#|><._*&=";
+            const std::string open_pair_symbols = "'\"`#";
+            const std::string delimiters = " \t\r\n\a";
 
             machine_state mst ;
 
             std::stringstream building_block;// = new std::stringstream;
-            for (char ch : *input_str){
-                size_t pos = special_symbols.find_first_of(ch);
-                if (pos != std::string::npos){ //if among special symbols
-                    switch (ch){
-                        case '#': {
-                            if ( !(mst.isBrace || mst.isDoubleBrace || mst.isReverceBrace) )
-                                mst.isComment = true;
-                            else
-                                building_block<<ch;
+
+
+            size_t char_index = 0;
+            size_t end_of_string = input_str->length();
+            char ch;
+
+            if (ss.good()) ch = (char)ss.get();
+            else std::cout<< "EMPTY STRING "<<std::endl;
+
+            while (ss.good()){
+
+
+                size_t founded_char_position = open_pair_symbols.find_first_of(ch);
+                if (founded_char_position != std::string::npos) {
+
+                    if (building_block.rdbuf()->in_avail() != 0) {
+                        tokens_vector->push_back(std::pair<string, char>(building_block.str(), 'S'));
+                        building_block.str("");
+                        mst.isDoubleBrace = false;
+                    }
+
+                    switch (ch) {
+
+                        case '"':{
+                            mst.isDoubleBrace = true;
+
+                            if (ss.good()) {
+                                ch = static_cast<char> (ss.get());
+
+                                while (ch != '"' && ss.good()) {
+                                    building_block << ch;
+                                    ch = static_cast<char> (ss.get());
+                                };
+
+                            }
+                            if (!ss.good()) {
+                                std::cout << "ERROR, can't find closing pair for opening char!" << std::endl;
+                               // return NULL;
+                            }
+                            else {
+                                tokens_vector->push_back(std::pair<string, char>( building_block.str(), '"' ));
+                                building_block.str("");
+                                mst.isDoubleBrace = false;
+                            }
                             break;
                         }
-                        case ' ':{
-                            if
-                        }
-                        default: printf("Error matching pattern occured\n");
+
+                        default: std::cout << "ERROR DECODING TOKENS" << std::endl;
+
                     }
+                    continue;
                 }
-                else{
+                else { //not braces
+
                     building_block << ch;
+                    std::cout << (int)ch << std::endl;
+
                 }
-
-
-
+                ch = static_cast<char> (ss.get()) ;
 
             }
-            */
+
+            if (building_block.rdbuf()->in_avail() != 0) {
+                tokens_vector->push_back(std::pair<string, char>(building_block.str(), 'k'));
+                building_block.str("");
+                mst.isDoubleBrace = false;
+            }
+
+
+//
+//            while (char_index < end_of_string){
+//                ch = (*input_str).at(char_index);
+//
+//                size_t founded_char_position = open_pair_symbols.find_first_of(ch);
+//                if (founded_char_position != std::string::npos) {
+//
+//                    switch (ch) {
+//
+//                        case '"':{
+//                            mst.isDoubleBrace = true;
+//                            ++char_index;
+//                            if (char_index >= end_of_string){
+//
+//                            }
+//                            ch = (*input_str).at(char_index);
+//
+//                            while (ch != '"' && char_index < end_of_string){
+//                                building_block << ch;
+//                                ++char_index;
+//                                ch = (*input_str).at(char_index);
+//                            };
+//                            if (char_index == end_of_string) {
+//                                std::cout << "ERROR, can't find closing pair for opening char!" << std::endl;
+//                                return nullptr;
+//                            }
+//
+//
+//                            else {
+//                                building_block.
+//                            }
+//                            break;
+//                        }
+//
+//                        default: std::cout << "ERROR DECODING TOKENS" << std::endl;
+//
+//                    }
+//                    continue;
+//                }
+//                else { //not braces
+//
+//                }
+//
+//
+//                /*
+//                founded_char_position = delimiters.find_first_of(ch);
+//                if (founded_char_position != std::string::npos){
+//                    if (mst.isIndependant())
+//
+//                        if (building_block.rdbuf()->in_avail() == 0) // check stringstream isEmpty
+//                            continue;
+//
+//                        else{ // flushing buffer into our tocken vector
+//
+//                            tokens_vector->push_back(std::pair(
+//                                    building_block.str(), mst.getToken()
+//                            ));
+//                            building_block.clear();
+//                    }
+//
+//                    if (isSpace(ch)){
+//                        building_block << ch;
+//                    }
+//                    continue;
+//                }
+//                */
+//
+//
+//                /*
+//                founded_char_position = special_symbols.find_first_of(ch);
+//                if (founded_char_position != std::string::npos){ //if among special symbols
+//
+//
+//                    switch (ch){
+//                        case '#': {
+//                            if ( !(mst.isInAnyBrace()) )
+//                                mst.isComment = true;
+//                            else
+//                                building_block<<ch;
+//                            break;
+//                        }
+//                        case ' ':{
+//                            if ( mst.isIndependant() ){
+//
+//                            }
+//                        }
+//                        default: printf("Error matching pattern occured\n");
+//                    }
+//                }
+//                else{
+//                    building_block << ch;
+//                }
+//
+//                continue;
+//
+//
+//                 */
+//                ++char_index;
+//            }
+
+
+
+            for (auto toks: *tokens_vector){
+                std::cout << "[" <<toks.first << "]" << " <" << toks.second << "> " << std::endl;
+            }
+
 
 
             input_str->append(" ");
